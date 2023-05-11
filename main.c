9d650d116f86faca7c920559755ceacc59dcae97
Christopher Kennedy <csk86.05.14@gmail.com>
2023-05-11 17:33:23 -0500

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+    if (NULL) {
+	printf("Uh oh...\n");
+	return -1;
+    }
+    
+    printf("LinkedList\n");
+    printf("HashMap\n");
+    printf("Breadth First Search\n");
+    printf("Depth First Search\n");
+
+    return 0;
+}
